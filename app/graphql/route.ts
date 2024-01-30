cdbae032ec7bbd11925ba11d020739defa2df5cf
Kyle Satti <k@kylejs.dev>
2024-01-30 07:55:53 -0500

    Try adding local landing page to prod

--- app/graphql/route.ts
+++ app/graphql/route.ts
@@ -3,11 +3,20 @@ import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
 import resolvers from "@/graphql/resolvers";
 import typeDefs from "@/graphql/schema";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+
+let plugins = [];
+if (process.env.NODE_ENV === "production") {
+  plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+} else {
+  plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
+  plugins,
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
